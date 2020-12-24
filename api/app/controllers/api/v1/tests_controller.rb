7e4f17aecb8a9eee34564fe42bff6a54d93494f7
yamagai <kohei.yamamoto919@gmail.com>
2020-12-25 00:01:18 +0900

    tekton-test2

--- api/app/controllers/api/v1/tests_controller.rb
+++ api/app/controllers/api/v1/tests_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TestsController < ApplicationController
       def index
-        render json: { status: 'SUCCESS', message: 'tekton-test3' }
+        render json: { status: 'SUCCESS', message: 'tekton-test4' }
       end
     end
   end
